73e5fbc4436ba91b3025a0c9fc79dcc40764b187
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-08-14 20:00:29 -0400

    all routes working properly with calls from Postman

--- server.js
+++ server.js
@@ -4,23 +4,47 @@ const mongoose = require('mongoose');
 
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
-const server = express();
+const server = express(); 
 
-// allow server to parse JSON bodies from POST/PUT/DELETE requests
+const Bear = require('./models');
+
 server.use(bodyParser.json());
 
+// POST /bears
+server.post('/bears', (req, res) => {
+  const { species, latinName } = req.body;
+  if (!species || !latinName) {
+    res.status(STATUS_USER_ERROR).json({ error: 'Please provide species and latinName' });
+    return;
+  }
+  const bear = new Bear({ species, latinName });
+  bear.save((err) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json(err);
+    } res.status(200).json(bear);
+  });
+});
 
+// GET /bears
+server.get('/bears', (req, res) => {
+  Bear.find({}, (err, bears) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json(err);
+    } res.status(200).json(bears);
+  });
+});
 
+// GET /bears/:id
+server.get('/bears/:id', (req, res) => {
+  const { id } = req.params;
+  Bear.findById(id, (err, bear) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json(err);
+    } res.status(200).json(bear);
+  });
+});
 
-
-
-// TODO: write your server code here
-
-
-
-
-
-
+// connect to mongo
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
   'mongodb://localhost/bears',
